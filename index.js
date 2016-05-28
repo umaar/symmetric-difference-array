47915b22526f7f367cbcbdc3797bbe2363b73628
Umar <umar.hansa@gmail.com>
2016-05-28 03:08:57 +0100

    alt approach

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 
 'use strict';
 
-function diffArray(arr1, arr2) {
+const includes = require('array-includes');
+
+function diffArray1(arr1, arr2) {
 	var newArr = [];
 	arr1.forEach(item => {
 		const index = arr2.indexOf(item);
@@ -15,5 +17,13 @@ function diffArray(arr1, arr2) {
 	return newArr.concat(arr2);
 }
 
-const result = diffArray([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]);
-console.log(result);
+function diffArray2(arr1, arr2) {
+	return arr1.concat(arr2).filter(item => {
+		return !includes(arr1, item) || !includes(arr2, item);
+	});
+}
+
+const result1 = diffArray1([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]);
+const result2 = diffArray2([1, 2, 3, 5, 6], [1, 2, 3, 4, 5]);
+console.log(result1);
+console.log(result2);
